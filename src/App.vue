fd2498b5ddc7456b812fd6d0d33f04dcd7cb2cea
Duane Faria <duane_gabriel@hotmail.com>
2024-07-10 02:33:28 -0300

    add search bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import Header from '@/components/header/Header.vue'
+</script>
 
 <template>
   <section class="container">
-    <header>header</header>
+    <header>
+      <input placeholder="search games" />
+    </header>
 
-    <aside>aside</aside>
+    <aside>Genres</aside>
 
     <main>main</main>
   </section>
@@ -17,10 +21,24 @@
   grid-template-rows: 2;
 }
 
+/* header */
 .container header {
   grid-column: 1 / 3;
   grid-row: 1;
+  display: flex;
 }
+
+.container header input {
+  background-image: url('./assets/icons/search.svg');
+  background-repeat: no-repeat;
+  /* background-position: left; */
+  padding-left: 40px;
+  border: none;
+  flex: 1;
+  border: 1px solid black;
+  border-radius: 15px;
+}
+/* end header */
 
 .container aside {
   grid-column: 1;
